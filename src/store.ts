008abccdc136ebb87266a3ecfd9a99704a9c47c1
Aleksander Brymora <aleksander.australia@gmail.com>
2022-02-18 18:12:45 +0100

    no defaults

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,18 @@
 import { writable } from 'svelte/store';
 
 export const details = writable({
-	clpl: '2730',
-	sopl: '5507',
-	po: '####',
-	souk: '64537',
-	customer: 'Cameron Shiels',
-	dealer: 'JCI Limited'
+	// clpl: '2730',
+	// sopl: '5507',
+	// po: '####',
+	// souk: '64537',
+	// customer: 'Cameron Shiels',
+	// dealer: 'JCI Limited',
+	clpl: '',
+	sopl: '',
+	po: '',
+	souk: '',
+	customer: '',
+	dealer: ''
 });
 
-export const partNames = writable<string[]>(['Frame', 'Hardware', 'Panel 1L']);
+export const partNames = writable<string[]>([]);
