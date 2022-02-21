40a8624c7604c5d060581dc997c3c03ea0ae115d
Aleksander Brymora <aleksander.australia@gmail.com>
2022-02-21 23:35:03 +0100

    removed dummy data

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,23 @@
 import { writable } from 'svelte/store';
 
-// export const details = writable({
-// 	clpl: '',
-// 	sopl: '',
-// 	po: '',
-// 	souk: '',
-// 	customer: '',
-// 	dealer: ''
-// });
-
-// export const partNames = writable<string[]>([]);
-
 export const details = writable({
-	clpl: '2730',
-	sopl: '5507',
-	po: '####',
-	souk: '64537',
-	customer: 'Cameron Shiels',
-	dealer: 'JCI Limited'
+	clpl: '',
+	sopl: '',
+	po: '',
+	souk: '',
+	customer: '',
+	dealer: ''
 });
 
-export const partNames = writable<string[]>(['Part', 'Something', 'some longer name']);
+export const partNames = writable<string[]>([]);
+
+// export const details = writable({
+// 	clpl: '2730',
+// 	sopl: '5507',
+// 	po: '####',
+// 	souk: '64537',
+// 	customer: 'Cameron Shiels',
+// 	dealer: 'JCI Limited'
+// });
+
+// export const partNames = writable<string[]>(['Part', 'Something', 'some longer name']);
